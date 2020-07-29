ca73d8906ac81231393c44b7637949fc92a888e7
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-29 16:47:44 +0530

    connect implemented

--- src/Components/CakeContainer.js
+++ src/Components/CakeContainer.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React from "react";
+import { connect } from "react-redux";
+import { buyCake } from "../redux";
 
-function CakeContainer() {
-    return (
-        <div>
-            <h2>Num of Cakes</h2>
-            <button>Buy Cake</button>
-        </div>
-    )
+function CakeContainer(props) {
+  return (
+    <div>
+      <h2>Num of Cakes - {props.numOfCakes}</h2>
+      <button onClick={props.buyCake} >Buy Cake</button>
+    </div>
+  );
 }
 
-export default CakeContainer
+// step1
+const mapStateToProps = (state) => {
+  return {
+    numOfCakes: state.numOfCakes,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyCake: () => dispatch(buyCake()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
